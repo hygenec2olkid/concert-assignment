6593de95cbadb30f8479510e996c423af625f226
Wirapat Prigphet <wirapat@sirisoft.co.th>
2026-02-18 17:10:58 +0700

    feat: implement responsesive layout

--- web/src/app/home/page.tsx
+++ web/src/app/home/page.tsx
@@ -3,14 +3,22 @@
 import Tab from "@/src/components/ui/Tab";
 import CreateForm from "@/src/components/features/home/createForm";
 import Overview from "@/src/components/features/home/overview";
+import SummaryBox from "@/src/components/features/home/summaryBox";
 
 export default function Home() {
   return (
-    <Tab
-      tabs={[
-        { label: "Overview", content: <Overview /> },
-        { label: "Create", content: <CreateForm /> },
-      ]}
-    />
+    <>
+      <div className="grid grid-cols-2 gap-3 gap-y-0 tablet:grid-cols-3 tablet:gap-10">
+        <SummaryBox value={10} type="total" />
+        <SummaryBox value={10} type="reserve" />
+        <SummaryBox value={10} type="cancel" />
+      </div>
+      <Tab
+        tabs={[
+          { label: "Overview", content: <Overview /> },
+          { label: "Create", content: <CreateForm /> },
+        ]}
+      />
+    </>
   );
 }
---
+++ web/src/components/features/home/summaryBox.tsx
@@ -0,0 +1,68 @@
+import { Box } from "@mui/material";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+
+type SummaryBoxType = "total" | "reserve" | "cancel";
+
+type SummaryBoxProps = {
+  value: number;
+  type: SummaryBoxType;
+};
+
+type SummaryBoxConfig = {
+  icon: React.ReactNode;
+  backgroundColor: string;
+  description: string;
+};
+
+const SummaryBoxConfig: Record<SummaryBoxType, SummaryBoxConfig> = {
+  total: {
+    icon: (
+      <PersonOutlineIcon
+        sx={{ fontSize: { xs: 25, sm: 30, lg: 45 }, color: "#fff" }}
+      />
+    ),
+    backgroundColor: "#0070A4",
+    description: "Total of seats",
+  },
+  reserve: {
+    icon: (
+      <EmojiEventsIcon
+        sx={{ fontSize: { xs: 25, sm: 30, lg: 45 }, color: "#fff" }}
+      />
+    ),
+    backgroundColor: "#00A58B",
+    description: "Reserve",
+  },
+  cancel: {
+    icon: (
+      <HighlightOffIcon
+        sx={{ fontSize: { xs: 25, sm: 30, lg: 45 }, color: "#fff" }}
+      />
+    ),
+    backgroundColor: "#E84E4E",
+    description: "Cancel",
+  },
+};
+
+export default function SummaryBox({ value, type }: SummaryBoxProps) {
+  const config = SummaryBoxConfig[type];
+  return (
+    <Box
+      sx={{
+        backgroundColor: config.backgroundColor,
+        borderRadius: "8px",
+        padding: { xs: "4px", sm: "4px", md: "8px", lg: "16px" },
+        marginBottom: "16px",
+        aspectRatio: "2 / 1",
+      }}
+    >
+      <div className="flex flex-col items-center justify-between h-full text-white desktop:gap-1 ">
+        {config.icon}
+        <p className="text-sm"> {config.description}</p>
+        <p className="text-lg tablet:text-xl desktop:text-5xl ">{value}</p>
+      </div>
+    </Box>
+  );
+}
